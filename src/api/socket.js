71324a59fbd9fd6e4ee8949db8e6911b3d8968a5
slimkhardani <khardenislim0@gmail.com>
2026-07-27 02:13:18 +0100

    solve

--- src/api/socket.js
+++ src/api/socket.js
@@ -4,4 +4,16 @@ const socket = io(import.meta.env.VITE_API_URL || 'http://localhost:3000', {
   autoConnect: false,
 });
 
+socket.on('connect', () => {
+  console.log('[socket] connected, id:', socket.id);
+});
+
+socket.on('disconnect', (reason) => {
+  console.log('[socket] disconnected:', reason);
+});
+
+socket.onAny((event, ...args) => {
+  console.log('[socket] event received:', event, args);
+});
+
 export default socket;
